d7a872237e86b2e592d554427361230c7309812e
Dmitry Shemyakin <68197037+Sh-Dmitry@users.noreply.github.com>
2023-04-07 20:31:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ks_2samp
-from scipy.stats import anderson_ksamp
+from scipy.stats import cramervonmises_2samp
+
+
 chat_id = 285100540 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     p = 0.02
-    res = anderson_ksamp(x, y)
+    res = cramervonmises_2samp(sample1, sample2)
     pval = res.pvalue
     if pval >= p:
         return False
