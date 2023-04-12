caacfe0b0f45cb6bdc04a2b27d3e7a856c427dc5
Dmitry Shemyakin <68197037+Sh-Dmitry@users.noreply.github.com>
2023-04-12 17:05:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import ks_2samp
 from scipy.stats import cramervonmises_2samp
-
+from scipy.stats import anderson_ksamp
 
 chat_id = 285100540 # Ваш chat ID, не меняйте название переменной
 
